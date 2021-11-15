51c6b52a9fd3a58c1453efcaca728e1aa0bd186f
Victor Bjørholm <Victor@fridayhome.net>
2021-11-15 16:49:31 +0100

    Forgot a file

--- RenameThisFileTo_credentials.php
+++ RenameThisFileTo_credentials.php
@@ -3,5 +3,6 @@ $sqlpassword = ""; //Your mysql password
 $sqlUsername ="root";//Your mysql sqlUsername
 $sqlhost = "localhost";
 $sqldb = "cryptotracker";//Db name
+$url = "http://victor.bjoerholm.dk";//Your url
 ?>
  
